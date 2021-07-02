238967233c06b1e5ca6ec8863af1c2d3ec3fc124
developer 203 <claudiostocco@gmail.com>
2021-07-02 09:47:16 -0300

    Instalação e preparação para styled-components e imagens.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/GlobalStyle";
+
 export function App() {
   return (
     <div className="App">
       <h1>Helo</h1>
+      <GlobalStyle/>
     </div>
   );
 }
